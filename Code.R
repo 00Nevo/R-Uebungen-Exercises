0856f87ae1f5c56b4868df436d1862c77cde3cd3
Gravity Online <william.mcintosh@zoho.com>
2025-03-18 20:19:26 +0100

    Create Code.R

---
+++ Code.R
@@ -0,0 +1,25 @@
+> n <- 15
+> n [1] 15
+Fehler: unerwartete numerische Konstante in "n [1] 15"
+> n
+[1] 15
+> n
+[1] 15
+> n
+[1] 15
+> 5 -> n
+> n
+[1] 5
+> n
+[1] 5
+> x <-1
+> X <- 10
+> x
+[1] 1
+> x
+[1] 1
+> X
+[1] 10
+> X
+[1] 10
+> 
