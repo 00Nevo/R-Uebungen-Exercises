34323ae1ccde5d1d0854b634c9aeee90d1d57cf0
Gravity Online <albertoborgato9@gmail.com>
2025-03-19 17:01:18 +0100

    Update Chap2_3The on-line help.r

--- Chap2_3The on-line help.r
+++ Chap2_3The on-line help.r
@@ -1,3 +1,12 @@
+## I finished reading working on the following.
+
+## Emmanuel Paradis - R for beginners. 
+
 #2.3 The on-line help
 # online help for given function
 
+> help("bs", try.all.packages = TRUE)
+> help("bs", package = "splines")
+> help.search("tree")
+> help.search("statistics")
+> help.search("tree", rebuild = TRUE)
