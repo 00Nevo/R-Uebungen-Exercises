3dfa92b58ef28514edff699c5ccf0b100ad3a795
Gravity Online <albertoborgato9@gmail.com>
2025-03-20 15:27:20 +0100

    Update Ch3 Data with R.r

--- Ch3 Data with R.r
+++ Ch3 Data with R.r
@@ -1,1 +1,28 @@
-##3 Data with R
+##
+3 Data with R
+
+3.1 Objects
+
+3.2 Reading data in a file
+
+3.3 Saving data
+
+3.4 Generating data
+
+3.5 Manipulating objects
+3.5.1 Creating objects
+
+3.5.2 Converting objects
+
+3.5.4 Accessing the values of an object: the indexing system
+
+3.5.5 Accessing the values of an object with names
+
+3.5.7 Arithmetics and simple functions
+
+3.5.8 Matrix computation
+
+
+
+
+
