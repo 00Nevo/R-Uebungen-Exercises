44556bb73af7d54c2c02e839c745e7295af7c7f6
Gravity Online <albertoborgato9@gmail.com>
2025-03-23 12:47:40 +0100

    Update Statistics-for- Khan-Academy.R

--- Statistics-for- Khan-Academy.R
+++ Statistics-for- Khan-Academy.R
@@ -2,3 +2,14 @@
  numbers <- c(3,5,6,7,9,8)
 > mean_value <- mean(numbers)
 > print(mean_value)
+[1] 6.333333
+##Calculate Median Value
+> numbers <- c(4,9,6,3,4,2)
+> median_value <- median (numbers)
+> print(median_value)
+[1] 4
+
+> numbers <- c(9,8,1,8,5)
+> median_value <- median (numbers)
+> print(median_value)
+[1] 8
