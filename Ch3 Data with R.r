b8022c2f91ee2fb59f975e90a892c028de126acb
Gravity Online <albertoborgato9@gmail.com>
2025-03-24 17:30:39 +0100

    Update Ch3 Data with R.r

--- Ch3 Data with R.r
+++ Ch3 Data with R.r
@@ -10,6 +10,22 @@
 3.4 Generating data
 
 3.5 Manipulating objects
+
+Expression.
+## i did get an error that exp1 was not defined when i tried to run the derivative example so i asked chat gpt and this is the solution offered
+> D(expl, "x")
+Error: Object 'expl' not found
+> exp1
+expression(x/(y + exp(z)))
+> D(expl, "x")
+Error: Object 'expl' not found
+
+> exp1 <- expression(x^2 + 3*x +2)
+> derivative <- D(exp1, "x")
+> print(derivative)
+2 * x + 3
+
+
 3.5.1 Creating objects
 
 3.5.2 Converting objects
