027012b3c270fc81f6a4c3e22b65dedfb423f568
Gravity Online <albertoborgato9@gmail.com>
2025-03-24 19:48:28 +0100

    Create CH1.Learning-R.Cotton.R

---
+++ CH1.Learning-R.Cotton.R
@@ -0,0 +1,13 @@
+## Learning R Richard Cotton - O´Reilly
+
+Introduction
+Chapter Goals
+What Is R?
+Installing R
+Choosing an IDE
+Emacs + ESS
+Eclipse/Architect
+RStudio
+Revolution-R
+Live-R
+Other IDEs and Editors
