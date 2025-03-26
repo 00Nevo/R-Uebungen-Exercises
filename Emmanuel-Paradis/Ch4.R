e6a8cfc82ea41f05ac5c470541e7eecde1332d9b
NEVO <albertoborgato9@gmail.com>
2025-03-26 12:25:02 +0100

    Create Ch4.R

---
+++ Emmanuel-Paradis/Ch4.R
@@ -0,0 +1,13 @@
+4 Graphics with R
+
+4.1 Managing graphics
+4.1.1 Opening several graphical devices
+4.1.2 Partitioning a graphic
+4.2 Graphical functions
+4.3 Low-level plotting commands
+4.4 Graphical parameters
+4.5 A practical example
+4.6 The grid and lattice packages
+
+..
+5 Statistical analyses with R
