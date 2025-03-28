672634d6abee3a6b16dd7f7435470bca3cb0c548
NEVO <albertoborgato9@gmail.com>
2025-03-28 13:57:36 +0100

    Create Exercises-Khan-Academy-Calculated-In-R.R

---
+++ Exercises-Khan-Academy-Calculated-In-R.R
@@ -0,0 +1,19 @@
+Arithmetic Mean.
+
+>numbers <- c(1,5,5,1)
+> mean_value<- mean(numbers)
+> print(mean_value)
+[1] 3
+> numbers <- c(3,2,26,9)
+> mean_value <- mean(numbers)
+> print (mean_value)
+[1] 10
+> numbers <- c(28,26,24)
+> mean_value <- mean(numbers)
+> print(mean_value)
+[1] 26
+> numbers <- c(4, 5, 10, 13)
+> mean_value <- (mean(numbers))
+> print(mean_value)
+[1] 8
+> 
