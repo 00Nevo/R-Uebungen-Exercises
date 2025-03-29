3abe8ed36496899df6b8627cbe99f41de26c2481
NEVO <albertoborgato9@gmail.com>
2025-03-29 13:55:08 +0100

    Update Ch1.R

--- Emmanuel-Paradis/Ch1.R
+++ Emmanuel-Paradis/Ch1.R
@@ -3,9 +3,12 @@ From Basics to Advanced Topics
 Emmanuel Paradis
 
 Preface vi
-1 Introduction 1
+Chapter 1 
+Introduction 
 1.1 Data Analysis, Open Source Software, and R . . . . . . . . . . . 1
 1.2 What Is (and What Is Not) in This Book . . . . . . . . . . . . . 2
 1.3 User Interfaces . . . . . . . . . . . . . . . . . . . . . . . . . . . 3
 1.4 Conventions . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 4
 
+Chapter 2
+Data Structures in R
