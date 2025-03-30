fda8b0049de832fb9057362e57976e36e1411076
NEVO <albertoborgato9@gmail.com>
2025-03-30 12:12:26 +0200

    Create Book2Ch2.R

---
+++ Emmanuel-Paradis/Book2Ch2.R
@@ -0,0 +1,12 @@
+2
+Data Structures in R
+2.1 General Considerations
+2.2 Modes
+2.2.1 Data Modes
+2.2.2 Other Modes
+2.2.3 NULL
+2.3.1 The Five Main Data Structures in R
+2.3.2 Attributes
+2.4 Exercises
+3
+Programming R Functions
