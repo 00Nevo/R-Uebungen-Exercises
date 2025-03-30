da3ba1f822d319d23ff7518a1959ecc784161668
NEVO <albertoborgato9@gmail.com>
2025-03-30 12:37:08 +0200

    Create Book2Ch3.R

---
+++ Emmanuel-Paradis/Book2Ch3.R
@@ -0,0 +1,18 @@
+3
+Programming R Functions
+
+3.1 Environments
+3.2 Arguments
+3.2.2 Missing and NULL Arguments
+3.2.3 The ‘...’ Argument
+3.3 Return Value
+3.3.1 Implicit and Explicit Returns
+3.3.2 Assignment and Superassignment
+3.4 Recursive Functions
+3.5 Classes and Generic Functions
+3.5.1 S3
+3.5.2 S4
+3.5.3 R6
+3.6 Exercises
+4
+Data Manipulation
