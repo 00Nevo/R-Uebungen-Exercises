562b276066c5fa39226e6c168d360c4176459f00
NEVO <albertoborgato9@gmail.com>
2025-03-30 13:24:15 +0200

    Update Book2Ch2.R

--- Emmanuel-Paradis/Book2Ch2.R
+++ Emmanuel-Paradis/Book2Ch2.R
@@ -5,6 +5,34 @@ Data Structures in R
 2.2.1 Data Modes
 2.2.2 Other Modes
 2.2.3 NULL
+##ChatGpt doing a walkthrough of part of the code of the book:
+z <- list(a = 1:2, b = 3)
+    Here, you create a list z with two named elements:
+        a contains the vector 1:2 (i.e., 1 2).
+        b contains the scalar value 3.
+z
+The list z will look like this:
+$a
+[1] 1 2
+$b
+[1] 3
+Next, you perform:
+z[1] <- list(NULL)
+What's happening:
+    z[1] refers to the first element of the list z, which is a.
+    By assigning NULL to z[1], you're removing the first element (a) but keeping the list structure. This essentially replaces the first element (a) with NULL.
+Result after z[1] <- list(NULL):
+z
+This will result in the following output:
+$a
+NULL
+$b
+[1] 3
+
+Explanation:
+    The first element a is now replaced by NULL.
+    The second element b remains unchanged.
+
 2.3.1 The Five Main Data Structures in R
 2.3.2 Attributes
 2.4 Exercises
