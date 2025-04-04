ce81cfb765db790cd5a50ce83d4d583ed7061ebf
NEVO <albertoborgato9@gmail.com>
2025-04-04 07:01:56 +0200

    Update Book2Ch4.R

--- Emmanuel-Paradis/Book2Ch4.R
+++ Emmanuel-Paradis/Book2Ch4.R
@@ -1,1 +1,28 @@
+4
+Data Manipulation
+
+4.1 Missing Data
+
+4.1.1 Missing Values in Data Files
+
+4.1.2 NA vs. NaN
+
+4.1.3 Missing Data in Data Analyses
+
+4.2 Logical Vectors
+
+4.3 Character Strings and Text
+
+4.3.1 Encodings
+
+4.3.2 Regular Expressions
+
+4.3.3 Approximate String Distance
+
+4.3.4 Building Strings in R vs. in Files
+
+4.4 Indexing
+
+4.5 Exercises
+
 
