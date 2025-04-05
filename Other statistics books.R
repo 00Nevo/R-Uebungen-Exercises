9d852d6501e7c57a2034ae00ff5b13ee3dff3960
NEVO <albertoborgato9@gmail.com>
2025-04-05 17:31:26 +0200

    Create Other statistics books.R

---
+++ Other statistics books.R
@@ -0,0 +1,20 @@
+Sozialwissenschaftliche Datenanalyse.
+Eine Einführung.
+
+Einleitung
+
+Zur Aufgabe der empirischen Sozialforschung und der quantitativen Datenanalyse im soziologischen Forschungsprozess
+
+Zur Datenlage: eigene Datenerhebungen oder Sekundäranalysen?
+
+Erste Schritte der Datenanalyse: Aufbereitung, Datenbeschreibung und bivariate Zusammenhänge
+
+Faktorenanalyse und Skalierung
+
+Lineare Regressionen
+
+Zur Logik der Datenanalyse: Welche Auswertungsstrategie passt am besten zu meiner Fragestellung?
+
+Logistische Regressionen
+
+Ein Ausblick auf fortgeschrittene statistische Analyseverfahren
