eac4245b1a1e9035005695b24bf14519af6ed1a6
NEVO <albertoborgato9@gmail.com>
2025-04-09 00:22:35 +0200

    Create Ch2-Book-Of-R.R

---
+++ Ch2-Book-Of-R.R
@@ -0,0 +1,19 @@
+2
+NUMERICS, ARITHMETIC,
+ASSIGNMENT, AND VECTORS
+
+2.1 R for Basic Math
+2.1.1 Arithmetic
+2.1.2 Logarithms and Exponentials
+2.1.3 E-Notation
+2.2 Assigning Objects
+2.3.1 Creating a Vector
+Sequences with seq
+Repetition with rep
+Sorting with sort
+Finding a Vector Length with length
+2.3.3 Subsetting and Element Extraction
+2.3.4 Vector-Oriented Behavior
+
+3
+MATRICES AND ARRAYS
