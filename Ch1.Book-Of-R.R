199c7e30cd6a7b847b0b27690405acf23fa28cb9
NEVO <albertoborgato9@gmail.com>
2025-04-08 16:35:44 +0200

    Create Ch1.Book-Of-R.R

---
+++ Ch1.Book-Of-R.R
@@ -0,0 +1,23 @@
+
+
+1
+GETTING STARTED
+
+1.1 Obtaining and Installing R from CRAN
+1.2 Opening R for the First Time
+1.2.1 Console and Editor Panes
+1.2.2 Comments
+1.2.4 Installing and Loading R Packages
+Installing Packages
+Updating Packages
+1.2.5 Help Files and Function Documentation
+1.3 Saving Work and Exiting R
+1.3.1 Workspaces
+1.3.2 Scripts
+1.4 Conventions
+1.4.1 Coding
+1.4.3 Exercises
+
+2
+NUMERICS, ARITHMETIC,
+ASSIGNMENT, AND VECTORS
