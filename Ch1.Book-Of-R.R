e03c323b2ce0391f5c36151dffda66262348f328
NEVO <albertoborgato9@gmail.com>
2025-04-09 22:37:18 +0200

    Update Ch1.Book-Of-R.R

--- Ch1.Book-Of-R.R
+++ Ch1.Book-Of-R.R
@@ -6,6 +6,14 @@ GETTING STARTED
 1.1 Obtaining and Installing R from CRAN
 1.2 Opening R for the First Time
 1.2.1 Console and Editor Panes
+
+By default, the R prompt that indicates R is ready and awaiting a command
+is a > symbol, after which a text cursor appears. To avoid confusion
+with the mathematical symbol for “greater than,” >, some authors (including
+me) prefer to modify this. A typical choice is R>, which you can set as follows:
+
+options(prompt="R>")
+
 1.2.2 Comments
 1.2.4 Installing and Loading R Packages
 Installing Packages
