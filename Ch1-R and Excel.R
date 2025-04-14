78f4c29db3acde08f91ea47222e31520377121e4
NEVO <albertoborgato9@gmail.com>
2025-04-14 13:00:40 +0200

    Create Ch1-R and Excel.R

---
+++ Ch1-R and Excel.R
@@ -0,0 +1,35 @@
+Statistics for Ecologists Using R and Excel.
+Data collection, exploration, analysis and presentation
+Mark Gardener
+
+1. Planning
+1.1 The scientific method
+    1.1.1 Planning stage
+    1.1.2 Recording stage
+    1.1.3 Analysis stage
+    1.1.4 Reporting stage
+1.2 Types of experiment/project
+1.3 Getting data – using a spreadsheet
+1.4 Hypothesis testing
+    1.4.1 Hypothesis and analytical methods
+1.5 Data types
+1.6 Sampling effort
+    1.6.1 Quadrat size
+    1.6.2 Species area rule
+    1.6.3 How many replicates?
+    1.6.4 Sampling method
+          Random sampling
+          Systematic sampling
+          Mixed sampling
+          Haphazard sampling
+    1.6.5 Precision and accuracy
+1.7 Tools of the trade
+1.8 The R program
+    1.8.1 Getting R
+    1.8.2 Installing R
+1.9 Excel
+    1.9.1 Installing the Analysis ToolPak
+    1.9.2 Other spreadsheets  
+Chapter 1: Summary
+
+2. Data recording
