1d340cd10a511b8f219c5568a2ee7b08ab9e4d49
NEVO <albertoborgato9@gmail.com>
2025-04-16 17:46:39 +0200

    Create Ch2.R

---
+++ Ch2.R
@@ -0,0 +1,8 @@
+2. Data recording
+2.1 Collecting data – who, what, where, when
+2.1.1 Biological data and science
+2.2 How to arrange data
+
+3. Beginning data exploration – using
+software tools
+
