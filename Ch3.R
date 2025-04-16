c3aac56c4a41f2decc18c17c92c3891cfa2bf533
NEVO <albertoborgato9@gmail.com>
2025-04-16 17:54:07 +0200

    Create Ch3.R

---
+++ Ch3.R
@@ -0,0 +1,36 @@
+3. Beginning data exploration – using
+software tools
+
+3.1 Beginning to use R
+3.1.1 Getting help
+3.1.2 Starting with R
+      Data names
+3.1.3 Inputting data
+      Combine c() command
+3.1.4 Seeing what data items you have
+      List items ls() command
+      Remove items rm() command
+3.1.5 Previous commands and R history
+      Saving history of commands
+      Saving the workspace
+      Saving a snapshot of your R session
+      Annotating work
+3.1.6 Additional analysis packages
+      Loading extra analysis routines using the library() command
+3.1.7 Exiting R q() command
+3.2 Manipulating data in a spreadsheet
+3.2.1 Sorting
+3.2.2 Data filtering
+3.2.3 Paste special
+3.2.4 File formats
+3.2.5 Opening a CSV file in Excel
+3.2.6 Lookup tables
+      Roman numerals
+      Using the IF() function to ignore blank cells
+3.2.7 Pivot tables
+      Customizing and saving pivot tables
+3.3 Getting data from Excel into R
+      The read.csv() command
+
+4. Exploring data – looking at
+numbers
